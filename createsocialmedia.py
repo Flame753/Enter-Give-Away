4f34c99b2a2108ebcd7a5271a9b704a1f8312248
Sergey Gurman <sgurman753@gmail.com>
2020-07-27 12:04:13 -0400

    added selenium and the path

--- createsocialmedia.py
+++ createsocialmedia.py
@@ -1,12 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
 class CreateSocialMedia:
-    def __init__(self, media_type, full_name, username, password):
+    def __init__(self, full_name, username, password):
         self.full_name = full_name
         self.username = username
-        self.type = media_type
         self.password = password
 
     def create_gmail(self):
-        pass
+        PATH = "/usr/local/bin/chromedriver"
+        driver = webdriver.Chrome(PATH)
+        driver.get("https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp")
 
 
 if __name__ == "__main__":
--- entergiveaway.py
+++ entergiveaway.py
@@ -1,7 +1,11 @@
 import geninfo
 import createsocialmedia as scm
 import accounts
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-
+PATH = "/usr/local/bin/chromedriver"
+driver = webdriver.Chrome(PATH)
+driver.get("https://gleam.io/E3TKa/scholomance-academy-preorder-bundle-giveaway")  # This URL could Change anytime
 if __name__ == "__main__":
     pass
