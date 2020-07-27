97ec2d6e0222aed7b13d37d133b862442271b96d
Sergey Gurman <sgurman753@gmail.com>
2020-07-27 19:45:04 -0400

    A start of the class

--- geninfo.py
+++ geninfo.py
@@ -1,9 +1,52 @@
+import time
+import random
+
+
 class GenInfo:
     def __init__(self):
-        raise NotImplementedError("Do not create raw GenInfo Objects.")
+        self.first_name = None
+        self.middle_name = None
+        self.last_name = None
+        self.gender = None
+        self.birth_year = None
+        self.one_adj_describe_yourself = None
+        self.two_adj_describe_yourself = None
+        self.location = None
+        self.occupation = None
+        self.something_you_like = None
+        self.username = None
+        self.password = None
+
+    def gen_random_person(self):
+        self.gen_full_name()
+        self.gen_gender()
+        self.gen_birth_year()
+        self.gen_username()
+        self.gen_password()
+
+        return {'first_name': self.first_name,
+                'middle_name': self.middle_name,
+                'last_name': self.last_name,
+                'gender': self.gender,
+                'birth_year': self.birth_year,
+                'username': self.username,
+                'password': self.password}
+
+    def gen_gender(self):
+        self.gender = random.choice(['male', 'female'])
 
     def gen_full_name(self):
-        pass
+        with open('names.txt', 'r') as file:
+            lis_of_names = file.readlines()
+            self.first_name = random.choice(lis_of_names).split()[0]
+            self.last_name = random.choice(lis_of_names).split()[1]
+            if self.first_name.find('-') != -1:
+                self.middle_name = self.first_name[self.first_name.find('-') + 1:]
+                self.first_name = self.first_name[:self.first_name.find('-')]
+
+    def gen_birth_year(self, min_age_group=0, max_age_group=65):
+        current_year = int(time.ctime().split()[-1])
+        self.birth_year = current_year - random.randint(min_age_group, max_age_group)
 
     def gen_username(self):
         pass
@@ -13,4 +56,10 @@ class GenInfo:
 
 
 if __name__ == "__main__":
-    pass
+    test = GenInfo()
+    test.gen_full_name()
+    test.gen_gender()
+    test.gen_birth_year(15)
+
+    print(test.first_name, test.middle_name, test.last_name)
+    print(test.gender, test.birth_year)
--- socialmedia.py
+++ socialmedia.py
@@ -1,10 +1,12 @@
+from string import capwords
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class SocialMedia:
-    def __init__(self, full_name, username, password):
-        self.full_name = full_name
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
         self.username = username
         self.password = password
         self.PATH = "/usr/local/bin/chromedriver"
@@ -14,6 +16,27 @@ class SocialMedia:
         self.driver.get(
             'https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp')
 
+        first_name = self.driver.find_element_by_id("firstName")
+        first_name.send_keys(self.first_name)
+        first_name.send_keys(Keys.RETURN)
+
+        last_name = self.driver.find_element_by_id("lastName")
+        last_name.send_keys(self.last_name)
+        last_name.send_keys(Keys.RETURN)
+
+        username = self.driver.find_element_by_id("username")
+        username.send_keys(self.username)
+        username.send_keys(Keys.RETURN)
+
+        password = self.driver.find_element_by_name("Passwd")
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+
+        confirm = self.driver.find_element_by_name("ConfirmPasswd")
+        confirm.send_keys(self.password)
+        confirm.send_keys(Keys.RETURN)
+
 
 if __name__ == "__main__":
-    pass
+    gmail = SocialMedia("Anna love", "anna12723492", "1234Anna")
+    gmail.create_gmail()
