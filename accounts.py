a4174f4d9faa75c84b810eac2ed1f650c2e99e0b
Sergey Gurman <sgurman753@gmail.com>
2020-07-28 17:56:48 -0400

    raise exception if changing ID

--- accounts.py
+++ accounts.py
@@ -37,12 +37,22 @@ class Accounts:
         try:
             self.cur.execute(f'SELECT {kind} FROM {self.table} WHERE id = ?;', (id,))
             data = self.cur.fetchone()
-            return data
+            return data[0]
         except sqlite3.OperationalError:
-            print(f"There is not such value: {kind}")
+            print(f"No such value: {kind}")
+
+    def update_info(self, id, kind, value):
+        try:
+            if kind == 'id':
+                raise Exception("Should not change the ID")
+            self.cur.execute(f'UPDATE {self.table} SET {kind} = ? WHERE id = ?;', (value, id))
+            self.conn.commit()
+        except sqlite3.OperationalError:
+            print(f"No such value: {kind}")
 
     def remove_account(self, id, password):
-        pass
+        self.cur.execute(f'DELETE FROM {self.table} WHERE id = ? AND number = ? AND pin = ?;', (id, card_number, pin))
+        self.conn.commit()
 
     def close_file(self):
         self.conn.commit()
@@ -57,5 +67,7 @@ if __name__ == "__main__":
     test = Accounts()
     #test.add_account(get_website_info())
     test.dis_all()
-    print(test.retrieve_info(1, "first_namse"))
-    print(test.retrieve_info(1, "gender"))
+    print(test.retrieve_info(1, "made_gmail"))
+    test.update_info(1, 'id', 2)
+    test.dis_all()
+
