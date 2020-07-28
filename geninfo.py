00cbc48d2c6f62d84a3768dc7b9efd5efc32e22a
Sergey Gurman <sgurman753@gmail.com>
2020-07-27 22:36:37 -0400

    switched names

--- geninfo.py
+++ geninfo.py
@@ -2,21 +2,15 @@ import time
 import random
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
-def get_website():
+def get_website_info():
     PATH = "/usr/local/bin/chromedriver"
     driver = webdriver.Chrome(PATH)
 
     driver.get("https://www.name-generator.org.uk/username/")
 
-    go_random = driver.find_element_by_id("fill_all")
-    go_random.click()
+    fill_website = driver.find_element_by_id("fill_all").click()
 
     data = {}
     fields = ['first_name', 'middle_name', 'surname', 'gender',
@@ -29,7 +23,8 @@ def get_website():
     driver.find_element_by_id("quick_submit").click()
     username = driver.find_elements_by_class_name("names")
 
+    return data
+
 
 if __name__ == "__main__":
-    test.website()
-    print(test.username)
+    get_website_info()
