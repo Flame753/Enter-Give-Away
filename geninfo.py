b0dd5c2e8a4c8c0f86a219171798621cba4eaa8c
Sergey Gurman <sgurman753@gmail.com>
2020-07-27 23:21:47 -0400

    commented the code

--- geninfo.py
+++ geninfo.py
@@ -10,17 +10,18 @@ from selenium.webdriver.support import expected_conditions as EC
 def get_website_info():
     PATH = "/usr/local/bin/chromedriver"
     driver = webdriver.Chrome(PATH)
-
     driver.get("https://www.name-generator.org.uk/username/")
 
     driver.find_element_by_id("fill_all").click()
 
+    # Adding the information into data
     data = {}
     fields = ['first_name', 'middle_name', 'surname', 'gender',
               'birth_year', 'adj1', 'adj2', 'location', 'job', 'likes']
     for field in fields:
         data[field] = driver.find_element_by_name(field).get_attribute("value")
 
+    # Setting up and going to the next page to create the username
     driver.find_element_by_name("allow_underscores").click()
     driver.find_element_by_name("allow_dots").click()
     driver.find_element_by_id("quick_submit").click()
@@ -28,6 +29,7 @@ def get_website_info():
         EC.presence_of_element_located((By.ID, "main"))
     )
 
+    # Getting the username from the site then add it to the data
     user = []
     usernames = main.find_elements_by_class_name("username_item")
     for username in usernames:
