0eaa51dbffcf3e0556a85bff6c91c4d51af633f8
Sergey Gurman <sgurman753@gmail.com>
2020-07-28 00:21:53 -0400

    edit commits and added password to the data

--- geninfo.py
+++ geninfo.py
@@ -14,14 +14,14 @@ def get_website_info():
 
     driver.find_element_by_id("fill_all").click()
 
-    # Adding the information into data
+    # Adding the Information into Data
     data = {}
     fields = ['first_name', 'middle_name', 'surname', 'gender',
               'birth_year', 'adj1', 'adj2', 'location', 'job', 'likes']
     for field in fields:
         data[field] = driver.find_element_by_name(field).get_attribute("value")
 
-    # Setting up and going to the next page to create the username
+    # Setting up and going to the next page to Create the Username
     driver.find_element_by_name("allow_underscores").click()
     driver.find_element_by_name("allow_dots").click()
     driver.find_element_by_id("quick_submit").click()
@@ -29,13 +29,20 @@ def get_website_info():
         EC.presence_of_element_located((By.ID, "main"))
     )
 
-    # Getting the username from the site then add it to the data
+    # Getting the Username from the Site then add it to the Data
     user = []
     usernames = main.find_elements_by_class_name("username_item")
     for username in usernames:
         user.append(username.text)
 
     data['username'] = random.choice(user)
+
+    # Creating and Adding a Password into Data
+    data['password'] = data.get('first_name') + ' ' + \
+                       data.get('adj1') + ' ' + \
+                       data.get('birth_year') + ' ' + \
+                       data.get('likes')
+
     return data
 
 
