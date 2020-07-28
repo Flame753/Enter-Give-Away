94d8777ee9f8ba3abf361843477f62fdf71a5328
Sergey Gurman <sgurman753@gmail.com>
2020-07-27 23:16:06 -0400

    added a why to get the user name

--- geninfo.py
+++ geninfo.py
@@ -2,6 +2,9 @@ import time
 import random
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_website_info():
@@ -10,7 +13,7 @@ def get_website_info():
 
     driver.get("https://www.name-generator.org.uk/username/")
 
-    fill_website = driver.find_element_by_id("fill_all").click()
+    driver.find_element_by_id("fill_all").click()
 
     data = {}
     fields = ['first_name', 'middle_name', 'surname', 'gender',
@@ -21,10 +24,18 @@ def get_website_info():
     driver.find_element_by_name("allow_underscores").click()
     driver.find_element_by_name("allow_dots").click()
     driver.find_element_by_id("quick_submit").click()
-    username = driver.find_elements_by_class_name("names")
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
 
+    user = []
+    usernames = main.find_elements_by_class_name("username_item")
+    for username in usernames:
+        user.append(username.text)
+
+    data['username'] = random.choice(user)
     return data
 
 
 if __name__ == "__main__":
-    get_website_info()
+    print(get_website_info())
