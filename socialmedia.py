def22b7faa93179865f27a4d7b3a21bcc4d2971e
Sergey Gurman <sgurman753@gmail.com>
2020-07-28 10:56:06 -0400

    added commits

--- socialmedia.py
+++ socialmedia.py
@@ -1,3 +1,4 @@
+import time
 from geninfo import get_website_info
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,23 +7,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def create_gmail(data, phone):
+def create_gmail(data, phone_number):
     PATH = "/usr/local/bin/chromedriver"
     driver = webdriver.Chrome(PATH)
     driver.get('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp')
 
+    # Waiting until see something on page
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "view_container"))
-    )
+        EC.presence_of_element_located((By.ID, "view_container")))
 
     # Entering all the Information into the Google Account to Create Account
     fields = {'firstName': 'first_name', 'lastName': 'surname', 'username': 'username',
               'Passwd': 'password', 'ConfirmPasswd': 'password'}
 
     for field, value in fields.items():
-        search = driver.find_element_by_id(field)
+        if field in ['Passwd', 'ConfirmPasswd']:
+            search = driver.find_element_by_name(field)
+        else:
+            search = driver.find_element_by_id(field)
         search.send_keys(data.get(value))
         search.send_keys(Keys.RETURN)
+
+    # Waiting until its verification page is loaded and enters phone number
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "view_container")))
+    search = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "phoneNumberId")))
+    search.send_keys(phone_number)
+    search.send_keys(Keys.RETURN)
 
 
 if __name__ == "__main__":
